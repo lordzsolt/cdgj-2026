c4e396c1228ede801715f07ad9d296c1da85b761
iffn <iffn1990@gmail.com>
2026-02-21 21:48:36 +0100

    Implemented improved vision

--- tech-art/Vision/VisionSync.gd
+++ tech-art/Vision/VisionSync.gd
@@ -2,10 +2,23 @@ extends Node
 
 @export var level_camera : Camera2D
 @export var view_camera : Camera2D
+@export var map_camera : Camera2D
 
-@export var player_level : Node2D
-@export var player_view : Node2D
+@export var level_viewport : SubViewport
+@export var view_viewport : SubViewport
+@export var map_viewport : SubViewport
+
+@export var level_player : Node2D
+@export var view_player : Node2D
+@export var map_player : Node2D
 
 func _process(delta: float) -> void:
-	player_view.transform = player_level.transform
+	view_camera.zoom = level_camera.zoom
+	map_camera.zoom = level_camera.zoom
 	
+	level_viewport.size = DisplayServer.window_get_size()
+	view_viewport.size = DisplayServer.window_get_size()
+	map_viewport.size = DisplayServer.window_get_size()
+	
+	view_player.transform = level_player.transform
+	map_player.transform = level_player.transform
