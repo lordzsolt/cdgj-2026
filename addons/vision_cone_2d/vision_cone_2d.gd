e4bf68a42adde452b935099233c8f475298065fd
iffn <iffn1990@gmail.com>
2026-02-21 11:45:43 +0100

    Revert "Fixed bug in library"
    This reverts commit 0321d595bc2be472404ef8800bebdbadc8954521.

--- addons/vision_cone_2d/vision_cone_2d.gd
+++ addons/vision_cone_2d/vision_cone_2d.gd
@@ -115,8 +115,7 @@ func _draw():
 func _update_collision_polygon():
 	if write_collision_polygon == null:
 		return
-	if _vision_points.size() > 2:
-		write_collision_polygon.polygon = _vision_points
+	write_collision_polygon.polygon = _vision_points
 
 func _update_render_polygon():
 	if write_polygon2d == null:
