dcd9df3f7c5f28acce1adbc5a7e45bc94222c977
Zsolt Kovacs <lordzsoltt@gmail.com>
2026-02-22 13:03:25 +0100

    Fix collision sliding with using custom polygon shape

---
+++ addons/vision_cone_2d/examples/collision_shape_2d.gd
@@ -0,0 +1,14 @@
+extends CollisionShape2D
+
+func _ready() -> void:
+	shape = create_polygon_circle(10, 16)
+
+func create_polygon_circle(radius: float, segments: int) -> ConvexPolygonShape2D:
+	var points := PackedVector2Array()
+	for i in segments:
+		var angle = TAU * i / segments
+		points.append(Vector2(cos(angle), sin(angle)) * radius)
+
+	var shape := ConvexPolygonShape2D.new()
+	shape.points = points
+	return shape
