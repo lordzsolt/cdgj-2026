e8c1e24968513b25bcc4a2977aced02e0cb73290
Fabio Piunti <fabio@piunti-se.ch>
2026-02-22 19:12:57 +0100

    fixed interaction manager if died in chaotic

--- global/interaction_manager/interaction_manager.gd
+++ global/interaction_manager/interaction_manager.gd
@@ -4,21 +4,21 @@ extends Node
 
 const baset_text = "Press [E] to ";
 
-var active_areas = [];
+var active_area = null;
 var can_interact = true;
 
 func register_area(area: InteractionArea):
-	active_areas.append(area);
+	active_area = area;
 
 func unregister_area(area: InteractionArea):
-	var index = active_areas.find(area);
-	if (index != -1):
-		active_areas.remove_at(index);
+	active_area = null
 
 func _input(event: InputEvent) -> void:
 	if (event.is_action_pressed("interact") && can_interact):
-		if active_areas.size() > 0:
+		if active_area:
 			can_interact = false;
 
-			await active_areas[0].interact.call();
+			await active_area.interact.call();
 			can_interact = true;
+			
+			
